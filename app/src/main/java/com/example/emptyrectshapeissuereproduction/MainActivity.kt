225b81f827746fe52b2499ce5412c6d8e2f83e48
Jeisson Sachica <jeisson.sachica@gmail.com>
2020-10-22 15:27:21 -0500

    Create minimal reproducible example and adds a .gif demo

--- app/src/main/java/com/example/emptyrectshapeissuereproduction/MainActivity.kt
+++ app/src/main/java/com/example/emptyrectshapeissuereproduction/MainActivity.kt
@@ -2,12 +2,28 @@ package com.example.emptyrectshapeissuereproduction
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.Text
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Slider
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Stable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Rect
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Outline
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.platform.setContent
-import androidx.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Density
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.graphics.Color
+
 import com.example.emptyrectshapeissuereproduction.ui.EmptyRectShapeIssueReproductionTheme
 
 class MainActivity : AppCompatActivity() {
@@ -17,7 +33,7 @@ class MainActivity : AppCompatActivity() {
             EmptyRectShapeIssueReproductionTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    Screen(modifier = Modifier.fillMaxSize())
                 }
             }
         }
@@ -25,14 +41,27 @@ class MainActivity : AppCompatActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun Screen(modifier: Modifier = Modifier) {
+    var sliderValue by remember { mutableStateOf(1f) }
+    Column(modifier) {
+        Box(
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxWidth()
+                .clip(PercentageRectangleShape(sliderValue))
+                .background(Color.Gray)
+        )
+        Slider(
+            value = sliderValue,
+            onValueChange = {
+                sliderValue = it
+            }
+        )
+    }
 }
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    EmptyRectShapeIssueReproductionTheme {
-        Greeting("Android")
-    }
+@Stable
+class PercentageRectangleShape(private val percentage: Float): Shape {
+    override fun createOutline(size: Size, density: Density): Outline =
+        Outline.Rectangle(Rect(Offset.Zero, size * percentage))
 }
